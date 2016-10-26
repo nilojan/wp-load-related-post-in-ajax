226e18611b0e78c70a32e974cc806671fe90d2a2
Alvis Nilojan <nilojan@live.com>
2016-10-26 17:31:46 +0800

    JS changes
    clean up n fix

--- related-posts.js
+++ related-posts.js
@@ -1,36 +1,31 @@
 jQuery(document).ready(function($) {
-	
 	/**
-	 * but only if there is at least one page of new posts to load.
+	 * the magic starts
 	 */
 		// Insert the "Related Posts" link.
-		$('.entry-content').append('<p id="sra-related-posts"><a>See Related Articles</a></p><div id="respo"></div>');
+		$('.entry-content').append('<p id="sra-related-posts"><a href="">See Related Articles</a></p><div id="respo"></div>');
 			
 		// Remove the traditional navigation.
 		$('.navigation').remove();
-	
-	
 	/**
 	 * Load new posts when the link is clicked.
 	 */
- 
-        //$.ajaxSetup({cache:false});
         $("#sra-related-posts a").click(function(){
-            //var post_link = window.location.href;
-			//alert(post_link);
+			// get the post id as Int
 			var pido = parseInt(sra_related.poid);
+			// create data array with the action and the post id
 			var data = {
 				'action': 'my_related_posts',
 				'pido': pido
 			};
-		jQuery.post(sra_related.ajax_url, data, function(response) {
-			//alert('Got this from the server: ' + response);
-			$("#sra-related-posts").remove();
-			$("#respo").html(response);
-		});
-            //$(this).text("Related Articles loading");
-           // $(this).load(ajaxurl);
-		   
-        return true;
+			// ajax request start
+			jQuery.post(sra_related.ajax_url, data, function(response) {
+				// hide the button
+				console.log(response);
+				$("#sra-related-posts").remove();
+				// show the response on the respo div
+				$("#respo").html(response);
+			});		   
+			return false;
         });
  });
